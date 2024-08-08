13a7374a86504fd460ba9861bd89839e1c3366b5
leonleerl <leon.runlongli@gmail.com>
2024-08-08 22:55:18 +0800

    完成卡片的左右排布

--- src/pages/Game/HiraganaGame/index.tsx
+++ src/pages/Game/HiraganaGame/index.tsx
@@ -1,4 +1,3 @@
-
 import Card from "@/components/Card";
 import { AppDispatch, RootState } from "@/store";
 import { getCardList } from "@/store/modules/cardStore";
@@ -12,16 +11,23 @@ const HiraganaGame = () => {
     useEffect(()=>{
         dispatch(getCardList());
     }, [dispatch])
-    const cardList = useSelector((state: RootState) => state.card.cardList);
+    const cardList = useSelector((state: RootState) => state.card.cardList).slice(0,5);
+
+    if (cardList.length === 0){
+        return <div>Loading...</div>
+    }
 
     return <>
-    <div>
-    Hiragana Game
-    </div>
-    <div>
-        <ul>
-            {cardList.map(card => <li key={card.id}><Card card={card}/></li>)}
-        </ul>
+    <div className="flex justify-center items-center p-4">
+
+        <div className="flex-shrink-0 mx-20">
+            <Card card={cardList[2]}/>
+        </div>
+        <div className="flex flex-col space-y-4">
+            <ul>
+                {cardList.map(card => <li key={card.id}><Card card={card}/></li>)}
+            </ul>
+        </div>
     </div>
     </>
 }
