c0a503aeefb1b6556ca6d6ceffa27f50cdb1e463
leonleerl <leon.runlongli@gmail.com>
2024-08-15 23:23:37 +0800

    添加router懒加载

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,7 +1,9 @@
 import { createBrowserRouter } from 'react-router-dom'
-import Home from '../pages/Home'
-import Game from '../pages/Game'
 import Layout from '@/pages/Layout'
+import { lazy, Suspense } from 'react'
+
+const Home = lazy(() => import("@/pages/Home"))
+const Game = lazy(() => import("@/pages/Game/HiraganaGame"))
 
 
 const router = createBrowserRouter([
@@ -11,11 +13,17 @@ const router = createBrowserRouter([
     children:[
       {
         index:true,
-        element:<Home/>
+        element:
+          <Suspense fallback={"加载中"}>
+            <Home/>
+        </Suspense>
     },
        {
         path:'/game',
-        element: <Game />,
+        element: 
+        <Suspense fallback={"加载中"}>
+          <Game/>
+        </Suspense>
       }
     ]
   },
